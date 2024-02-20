5ed1c305dfd400e00068ce6299eea9832d226e2b
john <johngemmell124@gmail.com>
2024-02-20 15:12:16 -0500

    labs

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,7 +7,8 @@ function Labs() {
     <div className="container-fluid">
       <h1>Labs</h1>
       <Nav />
-      <Link to="/Labs/a3">Assignment 3</Link> |
+      <Link to="/Labs/a3"> Assignment 3 </Link>|
+      <Link to="/Labs/a4"> Assignment 4 </Link>
       <Routes>
         <Route path="/a3/*" element={<Assignment3 />} />
       </Routes>
