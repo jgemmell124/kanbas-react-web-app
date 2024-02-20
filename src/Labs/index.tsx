634c777b032d664ea1160e67375bbefcf5ded0ef
john <johngemmell124@gmail.com>
2024-02-20 14:15:12 -0500

    lab done

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,13 +1,18 @@
-import Assignment3 from './a3';
-import JavaScript from './a3/JavaScript';
+import { Link, Route, Routes } from 'react-router-dom';
+import Assignment3 from "./a3";
+import Nav from "../Nav";
 
 function Labs() {
-   return (
-    <div>
-      <Assignment3/>
-      <JavaScript/>
+  return (
+    <div className="container-fluid">
+      <h1>Labs</h1>
+      <Nav />
+      <Link to="/Labs/a3">Assignment 3</Link> |
+      <Routes>
+        <Route path="/a3/*" element={<Assignment3 />} />
+      </Routes>
     </div>
-   );
+  );
 }
 
 export default Labs;
