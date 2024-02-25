9bdf13231a68af219f57af2490e8ce9a5fbdfe5c
john <johngemmell124@gmail.com>
2024-02-24 22:05:05 -0500

    cleanup

--- test.js
+++
@@ -1,8 +0,0 @@
-const alice = {  first: 'Alice',  last: 'Wonderland',  salary: 100000};
-
-alice['salary'] = alice.salary + 10000;
-console.log(alice['salary']);
-
-alice.salary += 10000;
-
-alice.['salary'] += 10000;
